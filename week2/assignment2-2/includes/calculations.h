8cb760512bd5e2c71ceecd91a4375d6be7d4a2ea
Elisa <elisa.harjunpaa@brightslearning.com>
2023-06-20 18:42:21 +0300

    Assignment 2.2/week2 done!

---
+++ week2/assignment2-2/includes/calculations.h
@@ -0,0 +1,14 @@
+#ifndef CALCULATIONS_H
+#define CALCULATIONS
+#include <iostream>
+#include <cassert>
+#include <cmath>
+
+void get_numbers(double& num1, double& num2);
+bool is_digit(int num);
+void addition(double& num1, double& num2);
+void subtraction(double& num1, double& num2);
+void multiplication(double& num1, double& num2);
+void division(double& num1, double& num2);
+
+#endif
